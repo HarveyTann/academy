27a31e06295967dc01404b60bfbd07119b2740b4
HarveyTann <harvey@ozbgroup.com>
2021-01-14 09:20:51 +0800

    disc and notif done

--- models/courses.py
+++ models/courses.py
@@ -2,6 +2,7 @@ from odoo import models, fields
 
 class Courses(models.Model):
     _name = 'academy.courses'
+    _inherit = 'mail.thread'
 
     name = fields.Char()
     teacher_id = fields.Many2one('academy.teachers', string="Teacher")
